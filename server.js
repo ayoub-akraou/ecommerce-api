dcc965a4f73d350d558b0f7399be802eecdcf7e0
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-03 13:30:10 +0100

    feat: add morgan middleware

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 dotenv.config({ path: "config.env" });
 const app = express();
 
+// middlewares
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+// routes
 app.get("/", (req, res) => {
 	res.send("hello");
 });
