8f6cad4fad704bf4c9c719d07ce95a1c9fdb17fe
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-05 23:18:20 +0100

    feat(routes): add new end point GET /api/v1/categories/:id to get a specefic category by its id

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { createCategory, getCategories } = require("../services/categoryService");
+const { createCategory, getCategories, getCategory } = require("../services/categoryService");
 
 const router = express.Router();
 
 router.route("/").get(getCategories).post(createCategory);
-
+router.route("/:id").get(getCategory);
 module.exports = router;
