2e157c485f2bda1e6b03fdddeb5d7f45b3a6253c
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-05 20:04:46 +0100

    feat(services): add getCategories functionnality with its documentation

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,3 +1,16 @@
+const slugify = require("slugify");
+const CategoryModel = require("../models/categoryModel");
+const asyncHandler = require("express-async-handler");
+const categoryModel = require("../models/categoryModel");
+
+// @desc   Get all categories
+// @route  GET /api/v1/categories
+// @access Public
+exports.getCategories = asyncHandler(async (req, res) => {
+	const categories = await categoryModel.find();
+	res.json({ results: categories.length, data: categories });
+});
+
 // @desc   Create category
 // @route  /api/v1/categories
 // @access Private
