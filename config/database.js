42e638fd0c660b6cdee3e37e368210c7f10bbaa2
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-05 15:02:14 +0100

    feat(db): add database connection with Mongoose

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const dbConnection = () => {
+	mongoose
+		.connect(process.env.DB_URI)
+		.then((conn) => {
+			console.log("connected succesfuly");
+		})
+		.catch((err) => {
+			console.error(`connection failed: ${JSON.stringify(err)}`);
+			process.exit(1);
+		});
+};
+
+module.exports = dbConnection;
