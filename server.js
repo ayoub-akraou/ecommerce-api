eb339f1c14a1ac22d0213c24c718e1e40fef38d4
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-05 14:57:56 +0100

    refactor: move business logic from server.js into routes, services, and models

--- server.js
+++ server.js
@@ -2,25 +2,29 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const dbConnection = require("./config/database.js");
+
+const { Schema } = mongoose;
 
 dotenv.config({ path: "config.env" });
-mongoose
-	.connect(process.env.DB_URI)
-	.then((conn) => console.log(conn.connection.host))
-	.catch((err) => console.error(err.message));
 
 // express app
 const app = express();
+// connect to database
+dbConnection();
 
 // middlewares
+app.use(express.json());
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // routes
+const categoryRoute = require("./routes/categoryRoutes.js");
+app.use("/api/v1/categories", categoryRoute);
 app.get("/", (req, res) => {
 	res.send("hello");
 });
 
 const PORT = process.env.PORT;
 app.listen(PORT, (err) => {
-	console.log(`server startedon PORT ${PORT}!`);
+	console.log(`server started on PORT ${PORT}!`);
 });
