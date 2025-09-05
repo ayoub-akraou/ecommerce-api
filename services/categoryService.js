bbbc9df4342218563001d882869ca4fa11d7d2a5
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-05 20:03:38 +0100

    refactor(services): simplify createCategory function and add a breif documentation

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,12 +1,8 @@
-const categoryModel = require("../models/categoryModel.js");
-
-exports.createCategory = (req, res) => {
-	console.log(req.body.name);
-
-	const name = req.body.name;
-	const newCategory = new categoryModel({ name });
-	newCategory
-		.save()
-		.then((doc) => res.json(doc))
-		.catch((err) => console.error(err));
-};
+// @desc   Create category
+// @route  /api/v1/categories
+// @access Private
+exports.createCategory = asyncHandler(async (req, res) => {
+	const { name } = req.body;
+	const category = await CategoryModel.create({ name, slug: slugify(name) });
+	res.status(201).json(category);
+});
