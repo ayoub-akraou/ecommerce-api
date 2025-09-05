50483563063c9099262780912adfdf5cc58a4c33
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-05 20:01:36 +0100

    feat(model):  create a more clear and meaningfull category schema

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,5 +1,22 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const categorySchema = new Schema({ name: String });
+const { Schema } = require("mongoose");
+
+const categorySchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "the category name is required!"],
+			unique: [true, "the category name must be unique!"],
+			minLength: [3, "too short category name!"],
+			maxLength: [32, "too long category name!"],
+		},
+		slug: {
+			type: String,
+			lowercase: true,
+		},
+		image: String,
+	},
+	{ timestamps: true }
+);
 
 const categoryModel = mongoose.model("Category", categorySchema);
+module.exports = categoryModel;
