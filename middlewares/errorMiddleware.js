e64b0e4c1460438ee53615c1998f702869254219
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-11 08:26:15 +0100

    refactor(middleswares): add two error format, one for development mode and one for production mode

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,8 +1,15 @@
 const globalErrorMiddleware = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || "error";
-
-	res.status(400).json({ status: err.status, err, message: err.message, stack: err.stack });
+	if (process.env.NODE_ENV === "development") sendErrorForDev(err, res);
+	if (process.env.NODE_ENV === "production") sendErrorForProd(err, res);
 };
 
+function sendErrorForDev(err, res) {
+	res.status(err.statusCode).json({ status: err.status, err, message: err.message, stack: err.stack });
+}
+
+function sendErrorForProd(err, res) {
+	res.status(err.statusCode).json({ status: err.status, message: err.message });
+}
 module.exports = globalErrorMiddleware;
